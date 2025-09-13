8401abb6eead7348f62d861c852ddda5a10bfc4c
hoamz <dvhoaf@gmail.com>
2025-09-13 15:06:00 +0700

    learn jetpack ngay 13/9 Bottom sheet

--- app/src/main/java/com/hoamz/jpyt/MainActivity.kt
+++ app/src/main/java/com/hoamz/jpyt/MainActivity.kt
@@ -6,14 +6,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.credentials.CredentialManager
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.hoamz.jpyt.ui.theme.JPYTTheme
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -38,11 +58,69 @@ class MainActivity : ComponentActivity() {
                 Column(
                     modifier = Modifier.fillMaxSize()
                 ) {
-
+                    CustomSheet()
                 }
             }
         }
     }
+}
+
+
+//custom bottom sheet
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomSheet(
+    modifier: Modifier = Modifier
+) {
+    var isShowSheet by remember {
+        mutableStateOf(false)
+    }
+
+    val scope = rememberCoroutineScope()
+
+    val sheetState = rememberModalBottomSheetState()
+    val hideSheet = {
+        scope.launch {
+            sheetState.hide()
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(5.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                isShowSheet = !isShowSheet
+            }
+        ) {
+            Text(text = "Open")
+        }
+
+
+        if(isShowSheet){
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = {
+                    hideSheet().invokeOnCompletion {
+                        isShowSheet = false
+                    }
+                }
+            ) {
+                //noi dung ben trong sheet
+                Box(
+                    modifier = Modifier.size(300.dp)
+                        .background(color = Color.Red)
+                ){
+
+                }
+            }
+        }
+
+    }
+
 }
 
 
